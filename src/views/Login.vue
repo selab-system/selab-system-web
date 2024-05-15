7f1d282e3ca05886b9c184127035bcf40a02fa3c
treasure <a3309332517@163.com>
2024-05-15 19:36:04 +0800

    second commit

---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: 'MyLogin'
+}
+</script>
+<template>
+  <div class="Login">
+    <h1>实验室管理系统</h1>
+    <input type="text" placeholder="请输入你的账号">;
+    <input type="password" placeholder="请输入你的密码">
+    <router-link to="/home">登录</router-link>
+    <router-link to="/register">注册</router-link>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Myregister'
+}
+</script>
+
+<template>
+<div></div>
+</template>
+
+<style scoped>
+
+</style>
